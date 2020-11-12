c9a16abf67a361a9da4fa8e478d15bc1bc535c20
Artem <universalism123@gmail.com>
2020-11-12 12:43:21 +0200

    commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const url = 'https://jsonplaceholder.typicode.com/';
+const postsCntr = document.querySelector('.posts');
+
+const requestBtn = document.querySelector("#request");
+const idInput = document.querySelector('#id');
+
+requestBtn.addEventListener('click', main);
+
+async function main() {
+    const id = idInput.value;
+
+    if(!isNaN(id)) {
+        try {
+            const resPosts = await fetch(`${url}posts/${id}`);
+            const fetchedPost = await resPosts.json();
+            const resComments = await fetch(`${url}posts/${id}/comments`);
+            const comments = await resComments.json();
+
+            createPostWithComments(fetchedPost, comments);
+        } catch(error) {
+            console.log(new Error(error));
+        }
+    }
+}
+
+function createPostWithComments(post, comments) {
+    let {title, body} = post;
+    let titleElem = document.createElement('h4');
+    let bodyElem = document.createElement('p');
+    let commentElem = document.createElement('p');
+    const divOfComments = document.querySelector('.comments');
+
+    titleElem.textContent = title;
+    bodyElem.textContent = body;
+
+    postsCntr.append(titleElem);
+    titleElem.after(bodyElem);
+
+    if(comments.length != 0) {
+        commentElem.textContent = "Comments:";
+
+        for(let {email, body} of comments) {
+            let comment = document.createElement('p');
+            comment.innerHTML = email + "</br></br>" + body;
+            divOfComments.append(comment);
+        }
+
+        bodyElem.after(commentElem);
+    }
+}
